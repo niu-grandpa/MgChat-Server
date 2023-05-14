1be9805a76ad021e1b19edb0193b59c0845dc826
niu-grandpa <2864103063@qq.com>
2023-05-14 10:07:21 +0800

    初始化

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('*', (req, res, next) => {
   res.setHeader('Content-Type', 'text/html');
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
