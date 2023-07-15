f64eeca36ce8291519addea9722d05664c49af94
niu-grandpa <2864103063@qq.com>
2023-07-15 16:10:04 +0800

    update

--- src/routes/api/message/index.ts
+++ src/routes/api/message/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
-import { useDbCrud } from '../../../hooks';
-import { CollectionName } from '../../../types';
+import { useApiHandler, useDbCrud } from '../../../hooks';
+import {
+  CollectionName,
+  MessageCollection,
+  ResponseCode,
+} from '../../../types';
+import { signData, wrapperResult } from '../../../utils';
 
 const MessageApi = express.Router();
 const { read, update } = useDbCrud();
@@ -9,16 +14,73 @@ MessageApi
   /**
    * 获取用户与所有好友的聊天数据
    */
-  .get('/sync-friend-message', async (request, response) => {
+  .get('/sync-friend-messages', async (request, response) => {
     const { uid } = request.query;
     await read(
-      { table: CollectionName.MESSAGE_LOGS, filter: { uid }, response },
+      { table: CollectionName.MESSAGE_LOGS, response, filter: { uid } },
       'findAll'
     );
   })
+  /**
+   * 获取好友新消息
+   */
+  .get('/new-friend-messages', async (request, response) => {
+    const { uid } = request.query;
+    const list = await read(
+      { table: CollectionName.MESSAGE_LOGS, filter: { uid } },
+      'findAll'
+    );
+    if (list !== null) {
+      const all = list as MessageCollection[];
+      const newList = all.map(({ friend, logs }) => ({ friend, logs }));
+      // 加密传输
+      response.send(wrapperResult(signData(newList), ResponseCode.SUCCESS));
+    } else {
+      response.send(wrapperResult(null, ResponseCode.FAIL));
+    }
+  })
 
-  /**设置消息已读*/
-  .post('/have-read', (request, response) => {})
+  /**设置消息是否已读*/
+  .post('/action-read', (request, response) => {
+    const { uid, friend } = request.body.data;
+    const common = {
+      table: CollectionName.MESSAGE_LOGS,
+      filter: { $and: [{ uid }, { friend }] },
+    };
+
+    useApiHandler({
+      response,
+      required: {
+        target: request.body.data,
+        must: ['uid', 'friend'],
+      },
+      middleware: [
+        async () => {
+          const list = await read(common);
+          if (list !== null) {
+            for (const item of (list as MessageCollection).logs) {
+              if (item.read) continue;
+              item.read = true;
+            }
+            return list;
+          }
+          return false;
+        },
+        async (data: Promise<MessageCollection | boolean>) => {
+          const _data = await data;
+          let updated = false;
+          if (Array.isArray(_data)) {
+            await update({
+              ...common,
+              update: { $set: { logs: _data } },
+            });
+            updated = true;
+          }
+          response.send(wrapperResult(update, ResponseCode.SUCCESS));
+        },
+      ],
+    });
+  })
 
   /**
    * 删除用户某条消息记录
